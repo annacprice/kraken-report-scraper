3959ea740a1b28337d8299d184f5e8ad276b27dd
Anna Price <36504362+annacprice@users.noreply.github.com>
2019-10-29 20:02:58 +0000

    corrected filelist for csvwriter

--- csvbuilder.py
+++ csvbuilder.py
@@ -7,20 +7,21 @@ import os
 dictSpecies = defaultdict(dict)
 
 def csvbuild(input, output):
+    txtList = ['species name']
     for txt in os.listdir(input):
         fileExtension = txt.split(".")[-1]
         if fileExtension == "txt":
-	   txtFile = input + "/" + txt
-	   with open(txtFile, 'r') as file_in:
-	       csvInput = csv.reader(file_in, delimiter='\t')
-               for row in csvInput:
-                   dictSpecies[str(row[5])].update({txt : int(row[1])})
+            txtFile = input + "/" + txt
+            txtList.append(txtFile)
+	    with open(txtFile, 'r') as file_in:
+                csvInput = csv.reader(file_in, delimiter='\t')
+                for row in csvInput:
+                    dictSpecies[str(row[5])].update({txtFile : int(row[1])})
 
     with open(output + "/" + "krakensummary.csv", 'wb') as file_out:
-	fieldnames = ['name'] + [txt for txt in os.listdir(input)]
-	csvOutput = csv.DictWriter(file_out, fieldnames=fieldnames, restval=0)
-	csvOutput.writeheader()
-	for name in sorted(dictSpecies.keys()):
-	    nameValues = dictSpecies[name]
-	    nameValues['name'] = name
-	    csvOutput.writerow(nameValues) 
+        csvOutput = csv.DictWriter(file_out, fieldnames=txtList, restval=0)
+        csvOutput.writeheader()
+        for name in sorted(dictSpecies.keys()):
+            nameValues = dictSpecies[name]
+            nameValues['species name'] = name
+            csvOutput.writerow(nameValues)
